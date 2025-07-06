72517671d4f83127813ea20adbfa93ee0441252f
RakibMozumder <34626905+RakibMozumder@users.noreply.github.com>
2025-07-06 23:46:24 +0600

    vercel commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["https://library-management-system-client-ap.vercel.app"],
+    origin: ["https://librarymanagementsystemapi-alpha.vercel.app/"],
   })
 );
 app.use("/api/v1/books", bookRouter);
